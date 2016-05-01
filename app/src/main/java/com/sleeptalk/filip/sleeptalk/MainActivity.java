6c28ff2844313ccffc21aa02bf4a23ba68a28345
filip141 <filip141@gmail.com>
2016-05-02 01:01:09 +0200

    lfilter implemented

--- app/src/main/java/com/sleeptalk/filip/sleeptalk/MainActivity.java
+++ app/src/main/java/com/sleeptalk/filip/sleeptalk/MainActivity.java
@@ -3,10 +3,13 @@ package com.sleeptalk.filip.sleeptalk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,8 +26,15 @@ public class MainActivity extends AppCompatActivity {
         datafile = new FileSaver(this);
         WavFile file = new WavFile("/sdcard/Music/FB_MAT_1.wav");
         List<Double> wavBuffer = file.read();
+        Standarizer signalStandarizer = new Standarizer(wavBuffer, file.sampleRate);
+        signalStandarizer.decimate(16000);
+        signalStandarizer.standard();
+        signalStandarizer.lfilter(Arrays.asList(signalStandarizer.highPassCoeffs));
+        signalStandarizer.lfilter(Arrays.asList(new Double[]{1.0, -0.9735}));
+        Pair<List<Double>,Integer> signalParams = signalStandarizer.getSignal();
+        List<Double> stdSignal = signalParams.first;
         try {
-            datafile.save(wavBuffer);
+            datafile.save(stdSignal);
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ app/src/main/java/com/sleeptalk/filip/sleeptalk/Standarizer.java
@@ -0,0 +1,99 @@
+package com.sleeptalk.filip.sleeptalk;
+
+import android.util.Log;
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by filip on 01.05.16.
+ */
+public class Standarizer {
+
+    List<Double> signal;
+    int sampleRate;
+    public Double[] highPassCoeffs = new Double[]{ -0.001945207254063, -0.00411960524828,-0.009835398951842, -0.01695068201474,
+            -0.02273603655938,   0.9731072889955, -0.02273603655938, -0.01695068201474,
+            -0.009835398951842, -0.00411960524828,-0.001945207254063 };
+
+    public Standarizer(List<Double> signal, int sampleRate){
+        this.signal = signal;
+        this.sampleRate = sampleRate;
+    }
+
+    // Drop every sampleRate/newFreq sample
+    public void decimate(int newFreq){
+        int step;
+        List<Double> decimatedArray = new ArrayList<>();
+
+        // New frequency should be lower then Signal Sample Rate
+        if(newFreq < sampleRate){
+            // New frequency and old sample rate should be divisible
+            if(sampleRate % newFreq == 0){
+                step = sampleRate / newFreq;
+                for(int i=0; i < signal.size(); i=i+step){
+                    decimatedArray.add(signal.get(i));
+                }
+                signal = decimatedArray;
+                sampleRate = newFreq;
+            }
+            else{
+                throw new IllegalArgumentException("New Frequency and Sample Rate should be divisible");
+            }
+        }
+        else{
+            throw new IllegalArgumentException("Passed Sample Rate should be greater");
+        }
+    }
+
+    // Removes mean form signal and divide it by max value
+    public void standard(){
+        // Get maximum value
+        double maxValue = Collections.max(signal);
+        // Get average
+        double averageSum = 0;
+        for(Double elem: signal){
+            averageSum+=elem;
+        }
+        averageSum = averageSum/signal.size();
+
+        // Modify each element
+        for(int i=0; i < signal.size(); i++){
+            signal.set(i, (signal.get(i) - averageSum)/maxValue);
+        }
+    }
+
+    // FIR Filter
+    public void lfilter(List<Double> coeffs){
+        double signalSample;
+        double coefSample;
+        int convSize = coeffs.size() + signal.size() - 1;
+        double[] filtredSig = new double[convSize];
+        List<Double> outputSignal = new ArrayList<>();
+
+        // Filter loop
+        for(int n = 0; n < convSize; n++){
+            filtredSig[n] = 0;
+            for(int m = 0; m < coeffs.size(); m++){
+                if((n - m >= 0) && ((n - m) < signal.size())){
+                    signalSample = signal.get(n - m);
+                    coefSample = coeffs.get(m);
+                    filtredSig[n] += signalSample * coefSample;
+                }
+            }
+        }
+        // Rewrite to new list
+        for(double num: filtredSig){
+            outputSignal.add(num);
+        }
+        signal = outputSignal;
+    }
+
+    public Pair<List<Double>,Integer> getSignal(){
+        return new Pair<List<Double>,Integer>(signal, sampleRate);
+    }
+
+}
